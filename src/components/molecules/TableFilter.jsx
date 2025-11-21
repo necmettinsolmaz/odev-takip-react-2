648550f95c53ad62053be27649a6192ea3cc470f
necmettinsolmaz <necmettin.solmaz@icisleri.gov.tr>
2025-11-21 16:44:42 +0300

    feat:  filtrele ikonu eklendi

--- src/components/molecules/TableFilter.jsx
+++ src/components/molecules/TableFilter.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Input from '../atoms/Input'
 import Button from '../atoms/Button'
-
+import { LuFilter } from "react-icons/lu";
 const TableFilter = () => {
   return (
     <div className="filter-area bg-white p-2 rounded-lg shadow-md mb-2 flex flex-wrap gap-3 items-center"> 
         <Input placeholder={"Satır No Filtresi (Örn: 5, 7-17"} />
-        <Button name="Filtrele" renk="Yapmadı" />
+        <Button name="Filtrele" renk="Yapmadı" ><LuFilter /></Button>
     </div>
   )
 }
