7cf2cba8247712425cf160a23a456560cc82793b
necmettinsolmaz <necmettin.solmaz@icisleri.gov.tr>
2025-12-19 16:29:55 +0300

    feat: ödev başlığı alanına sınıf seçim selecti eklendi

--- src/components/molecules/TableHeader.jsx
+++ src/components/molecules/TableHeader.jsx
@@ -3,7 +3,8 @@ import { toPng } from 'html-to-image';
 import Button from '../atoms/Button'
 import { IoCameraOutline } from "react-icons/io5";
 import { FaRegFileExcel } from "react-icons/fa";
-const TableHeader = ({selectedClass, handlePNGExport}) => {
+import Select from '../atoms/Select';
+const TableHeader = ({selectedClass, handlePNGExport, onSelectChange, classes}) => {
  // 1. Ekran görüntüsü alınacak alanı işaretlemek için ref oluşturun
 
   // 2. Dışa Aktarma Fonksiyonu
@@ -15,8 +16,14 @@ const TableHeader = ({selectedClass, handlePNGExport}) => {
 
   return (
     <div >
-      <div  className="filter-area bg-white p-2 rounded-lg shadow-md mb-2 flex justify-end items-center"> 
-        
+      <div  className=" bg-white p-2 rounded-lg shadow-md mb-2 flex items-center"> 
+        <div className='flex flex-1 gap-2 w-full'>
+        <Select 
+        options={classes} 
+        onSelectChange={onSelectChange}
+        selectedValue={selectedClass}
+        />
+        </div>
         <div className='flex gap-2 no-export-items'>
           <Button name="Paylaş" renk="Mor"  onClick={onExportToPNG}><IoCameraOutline  /></Button>
           <Button name="Excel" renk="Gri"><FaRegFileExcel /></Button>
